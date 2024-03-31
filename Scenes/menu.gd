ecbd861a314090e9eedff81dc7016f15b08c91a0
MaxWesterlund <max.westerlund3@gmail.com>
2024-03-31 11:43:04 +0200

    tiles and joints

---
+++ Scenes/menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+var game_scene = preload("res://Scenes/main.tscn")
+
+func _on_start_pressed():
+	get_tree().change_scene_to_packed(game_scene)
+
+func _on_exit_pressed():
+	get_tree().quit()
