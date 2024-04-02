f6dacb8a5479cfc7330a425a1799a1ba432e8bdd
lomstfer <90508408+lomstfer@users.noreply.github.com>
2024-04-02 11:57:54 +0200

    spawn player

--- Scenes/map_generator.gd
+++ Scenes/map_generator.gd
@@ -1,6 +1,7 @@
 extends TileMap
 
 var bomb = preload("res://Scenes/bombo.tscn")
+var player = preload("res://Scenes/player.tscn")
 
 const TILE_IMG_COLOR = {
 	ground = Color8(0, 255, 0),
@@ -18,23 +19,26 @@ const TILE_ATLAS_COORDS = {
 
 func _ready():
 	var image = Globals.level_image
-
-	var start_position = spawn_tiles(image)
-	get_node("/root/Main/Player").position = start_position
-	
+	spawn_tiles_and_player(image)
 	spawn_bombs(image)
 
-func spawn_tiles(image: Image) -> Vector2:
+func spawn_tiles_and_player(image: Image):
 	var level_size = Vector2i(image.get_width(), image.get_height() - 1)
 	var pp: Vector2
+	var has_start = false
 	for y in range(level_size.y):
 		for x in range(level_size.x):
 			var color01 = image.get_pixel(x, y)
 			var color = Color8(color01.r8, color01.g8, color01.b8, color01.a8)
 			set_cell(0, Vector2i(x, y) - level_size/2, 0, atlas_coords_from_color(color))
 			if color == TILE_IMG_COLOR.start:
+				has_start = true
 				pp = position
-	return pp
+	if has_start:
+		var player = player.instantiate()
+		player.position = pp
+		add_child(player)
+		Events.spawn_player.emit(pp)
 
 func atlas_coords_from_color(color: Color) -> Vector2i:
 	if color.is_equal_approx(TILE_IMG_COLOR.ground):
